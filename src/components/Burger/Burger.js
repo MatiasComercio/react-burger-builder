d8f7bad9289f69f4c9a2aeed2efa206fea46ec51
Matias Comercio <mcomerciovazquez@gmail.com>
2020-03-22 19:28:30 -0500

    Add static ingredients from Burger Builder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,10 +3,22 @@ import classes from './Burger.module.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
+  const ingredients = props.ingredients;
+
+  const ingredientComponents = Object.keys(ingredients).map(ingredient => {
+    const ingredientCount = ingredients[ingredient];
+
+    const ingredientComponents = [...Array(ingredientCount)].map((_, i) => {
+      return <BurgerIngredient key={ingredient + i} type={ingredient} />
+    });
+
+    return ingredientComponents;
+  });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="meat" />
+      {ingredientComponents}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );

9a72a3ca6b55ba4c7a6dba4cb0858615e1f17229
Matias Comercio <mcomerciovazquez@gmail.com>
2020-04-05 16:44:32 -0500

    Add add ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,14 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
+// TODO: merge with ingredients
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+};
+
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -11,14 +19,35 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0
-    }
+    },
+    totalPrice: 4
+  };
+
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const newCount = oldCount + 1;
+    const newIngredients = {
+      ...this.state.ingredients
+    };
+    newIngredients[type] = newCount;
+
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + INGREDIENT_PRICES[type];
+
+    this.setState({totalPrice: newPrice, ingredients: newIngredients})
+  };
+
+  removeIngredientHandler = type => {
+
   };
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients}/>
-        <BuildControls />
+        <BuildControls
+          ingredientAdded={this.addIngredientHandler}
+        />
       </Aux>
     );
   }

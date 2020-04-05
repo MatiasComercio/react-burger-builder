1e31ac8a75f8c18ddca438daef9d63f21c32a824
Matias Comercio <mcomerciovazquez@gmail.com>
2020-04-05 16:30:57 -0500

    Add Build Controls UI

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,18 +2,37 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = props => {
-  const ingredients = props.ingredients;
+const i18n = {
+  err: {
+    noIngredients: {
+      msg: {
+        en: 'Please start adding ingredients!'
+      }
+    }
+  }
+};
 
-  const ingredientComponents = Object.keys(ingredients).map(ingredient => {
-    const ingredientCount = ingredients[ingredient];
+const lang = 'en';
 
-    const ingredientComponents = [...Array(ingredientCount)].map((_, i) => {
+const ingredientToComponent = (ingredient, ingredientCount) => {
+  const ingredientComponents = [...Array(ingredientCount)]
+    .map((_, i) => {
       return <BurgerIngredient key={ingredient + i} type={ingredient} />
     });
 
-    return ingredientComponents;
-  });
+  return ingredientComponents;
+}
+
+const burger = props => {
+  const ingredients = props.ingredients;
+
+  let ingredientComponents = Object.keys(ingredients)
+    .map(i => ingredientToComponent(i, ingredients[i]))
+    .reduce((mem, el) => mem.concat(el), []);
+
+  if (ingredientComponents.length === 0) {
+    ingredientComponents = <p>{i18n.err.noIngredients.msg[lang]}</p>;
+  }
 
   return (
     <div className={classes.Burger}>

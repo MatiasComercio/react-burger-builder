d093146bd7bb6cc2f1e57f3cfcc9bd543835791e
Matias Comercio <mcomerciovazquez@gmail.com>
2020-05-10 16:52:30 -0500

    Add order creation POST

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 // TODO: merge with ingredients
 const INGREDIENT_PRICES = {
@@ -70,8 +71,26 @@ class BurgerBuilder extends Component {
     this.setState({purchasing: false});
   };
 
-  purchaseContinueHandler = () => {
-    alert('Continue');
+  purchaseContinueHandler = async () => {
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice, // TODO: should recalculate in order not to trust the FE
+      customer: {
+        name: 'Max',
+        address: {
+          street: 'Test 1',
+          zipCode: '41351',
+          country: 'US',
+        },
+        deliveryMethod: 'fastest'
+      }
+    };
+    try {
+      const response = await axios.post('/orders.json', order);
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   render() {
